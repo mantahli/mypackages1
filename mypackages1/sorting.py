64618769ce1918462ae77e8217e151ae8750d7d0
mantahli <48723109+mantahli@users.noreply.github.com>
2019-03-22 11:40:35 +0200

    Update sorting.py

--- mypackages1/sorting.py
+++ mypackages1/sorting.py
@@ -12,33 +12,33 @@ def bubble_sort(items):
     return items
 
 def merge_sort(items):
-    if len(items)>1:
-        mid=len(items)//2
-        myL=items[:mid]
-        myR=items[mid:]
-        merge_sort(myL)
-        merge_sort(myR)
-
-        i=j=k=0
-
-        while i < len(myL) and j < len(myR):
-            if myL[i] < myR[j]:
-                items[k] = myL[i]
-                i+=1
-            else:
-                items[k] = myR[j]
-                j+=1
-            k+=1
-        while i < len(myL):
-            items[k] = myL[i]
-            i+=1
-            k+=1
-        while j < len(myR):
-            items[k] = myR
-            j+=1
-            k+=1
-    return items
-
+  """ Return array of items, sorted in ascending order """
+  if len(items) >1:
+      mid = len(items)//2 #Finding the mid of the array
+      L = items[:mid] # Dividing the array elements
+      R = items[mid:] # into 2 halves
+      merge_sort(L) # Sorting the first half
+      merge_sort(R) # Sorting the second half
+      i = j = k = 0
+      # Copy data to temp arrays L[] and R[]
+      while i < len(L) and j < len(R):
+          if L[i] < R[j]:
+              items[k] = L[i]
+              i+=1
+          else:
+              items[k] = R[j]
+              j+=1
+          k+=1
+      # Checking if any element was left
+      while i < len(L):
+          items[k] = L[i]
+          i+=1
+          k+=1
+      while j < len(R):
+          items[k] = R[j]
+          j+=1
+          k+=1
+  return items
 
 
 
